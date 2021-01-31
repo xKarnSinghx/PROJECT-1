033cf578f7564bbffa3a9a559e55df922eaaf660
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-01-31 21:35:57 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -1,1 +1,25 @@
 
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const checkAuth = require('./middleware/check-auth');
+
+mongoose.connect('mongodb://127.0.0.1/videoServer', {
+  useCreateIndex: true, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use('/api/videos', express.static('media/uploads'));
+app.use('/api/signIn', require('./routes/signIn'));
+app.use('/api/signUp', require('./routes/signUp'));
+app.use('/api/upload', checkAuth, require('./routes/upload'));
+
+module.exports = app;
