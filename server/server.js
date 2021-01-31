cf85c5cabfdf7ffa59de34f875ff41198898fd77
KARN <57630022+MrUltimate-Karn@users.noreply.github.com>
2021-01-31 21:33:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
 
+const http = require('http');
+const app = require('./App');
+
+const config = require('./config/default');
+
+const port = process.env.PORT || config.port;
+
+const server = http.createServer(app);
+
+server.listen(port);
